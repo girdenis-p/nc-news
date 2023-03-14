acdd8e971f18f98c420980df1b54f29ffa0210b5
girdenis-p <girdenis.p@gmail.com>
2023-03-14 14:15:41 +0000

    implement a user context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
+import UserInfo from "./UserInfo"
+
 import "./Header.css"
+import { useContext } from "react";
+import { UserContext } from "../contexts/User";
 
 function Header() {
+  const { loggedInUser } = useContext(UserContext);
+
   return (
     <header className="Header">
       <h1>NC News</h1>
+      <section className="LoggedInInfo">
+        <UserInfo username={loggedInUser} precedingText="Logged in as" />
+      </section>
     </header>
   )
 }
