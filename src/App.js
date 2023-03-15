77d3512f48def91a675ca23a19e9660cc988b75f
girdenis-p <girdenis.p@gmail.com>
2023-03-15 09:02:12 +0000

    add responsive nav bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
 import Articles from './components/Articles';
 import Header from './components/Header';
+import ArticlePage from './components/ArticlePage';
+import Nav from './components/Nav';
 
 import { Routes, Route } from 'react-router-dom';
-import ArticlePage from './components/ArticlePage';
 
+import './App.css';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Nav />
       <Routes>
         <Route path="/articles/:articleId" element={<ArticlePage />} />
         <Route path="/" element={<Articles />} />
